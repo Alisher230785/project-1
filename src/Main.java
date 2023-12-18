0ade0a4091d281a4497ecfea22a3ec8b87e92046
alishershayakhmetov <shayakhmetovalisher1@gmail.com>
2023-12-18 08:44:36 +0600

    203

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         int b = in.nextInt();
-        System.out.println(a+b);
+        System.out.println(a*b);
     }
 }
